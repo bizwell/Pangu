928a84d7e54c55d75605e8860a720318600263eb
songxiang <songxiang@niwodai.net>
2017-08-04 16:13:30 +0800

    去除 Waning

--- util/src/main/java/com/joindata/inf/common/util/log/MetricsAspect.java
+++ util/src/main/java/com/joindata/inf/common/util/log/MetricsAspect.java
@@ -1,7 +1,8 @@
 package com.joindata.inf.common.util.log;
 
-import com.joindata.inf.common.basic.exceptions.BizException;
-import com.joindata.inf.common.util.basic.JsonUtil;
+import java.lang.annotation.Annotation;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -9,8 +10,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.annotation.Configuration;
 
-import java.lang.annotation.Annotation;
-import java.util.concurrent.ConcurrentHashMap;
+import com.joindata.inf.common.basic.exceptions.BizException;
+import com.joindata.inf.common.util.basic.JsonUtil;
 
 /**
  * 日志切面类
@@ -20,45 +21,58 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Aspect
 @Configuration
-public class MetricsAspect {
+public class MetricsAspect
+{
 
-    public MetricsAspect() {
+    public MetricsAspect()
+    {
     }
 
     @Around("@annotation(com.joindata.inf.common.util.log.Metrics))")
-    public Object around(ProceedingJoinPoint point) throws Throwable {
+    public Object around(ProceedingJoinPoint point) throws Throwable
+    {
 
-        MethodSignature signature = (MethodSignature) point.getSignature();
+        MethodSignature signature = (MethodSignature)point.getSignature();
 
         Logger logger = getLogger(signature);
 
         Metrics metrics = signature.getMethod().getAnnotation(Metrics.class);
-        if (metrics == null) {
+        if(metrics == null)
+        {
             metrics = defaultMetrics;
         }
         String metricsName = signature.getDeclaringTypeName() + " - " + signature.getName();
         String inputJson = null;
-        //记录入参
-        if (metrics.logInput()) {
+        // 记录入参
+        if(metrics.logInput())
+        {
             inputJson = JsonUtil.toJSON(point.getArgs());
             logger.info("{} 调用【{}】- 参数为【{}】", metrics.desc(), metricsName, inputJson);
         }
-        //执行业务
-        try {
+        // 执行业务
+        try
+        {
             Object result = point.proceed();
-            //记录返回值
-            if (metrics.logOutput()) {
+            // 记录返回值
+            if(metrics.logOutput())
+            {
                 logger.info("{} 调用【{}】成功 - 返回值为【{}】", metrics.desc(), metricsName, JsonUtil.toJSON(result));
             }
             return result;
-        } catch (BizException be) {
-            if (StringUtils.isEmpty(inputJson)) {
+        }
+        catch(BizException be)
+        {
+            if(StringUtils.isEmpty(inputJson))
+            {
                 inputJson = JsonUtil.toJSON(point.getArgs());
             }
             logger.error("{} 调用【{}】业务异常 - 【参数 = {}】-【异常信息:code = {}, msg = {}】", metrics.desc(), metricsName, inputJson, be.getErrorEntity().getCode(), be.getErrorEntity().getMessage());
             throw be;
-        } catch (Exception e) {
-            if (StringUtils.isEmpty(inputJson)) {
+        }
+        catch(Exception e)
+        {
+            if(StringUtils.isEmpty(inputJson))
+            {
                 inputJson = JsonUtil.toJSON(point.getArgs());
             }
             logger.error("{} 调用【{}】系统异常 - 【参数={}】", metrics.desc(), metricsName, inputJson, e);
@@ -66,51 +80,61 @@ public class MetricsAspect {
         }
     }
 
-    private Logger getLogger(MethodSignature signature){
+    private Logger getLogger(MethodSignature signature)
+    {
         Logger logger = cachedLoggers.get(signature.getMethod().getDeclaringClass());
-        if(logger == null){
+        if(logger == null)
+        {
             logger = Logger.newLogger(signature.getMethod().getDeclaringClass());
             cachedLoggers.put(signature.getMethod().getDeclaringClass(), logger);
         }
         return logger;
     }
 
-    private static ConcurrentHashMap<Class, Logger> cachedLoggers = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<Class<?>, Logger> cachedLoggers = new ConcurrentHashMap<>();
 
-    private static final Metrics defaultMetrics = new Metrics() {
+    private static final Metrics defaultMetrics = new Metrics()
+    {
 
         @Override
-        public Class<? extends Annotation> annotationType() {
+        public Class<? extends Annotation> annotationType()
+        {
             return null;
         }
 
         @Override
-        public String desc() {
+        public String desc()
+        {
             return "";
         }
 
         @Override
-        public boolean logInput() {
+        public boolean logInput()
+        {
             return true;
         }
 
         @Override
-        public boolean logOutput() {
+        public boolean logOutput()
+        {
             return true;
         }
 
         @Override
-        public boolean ignoreBusErr() {
+        public boolean ignoreBusErr()
+        {
             return false;
         }
 
         @Override
-        public boolean ignoreSysErr() {
+        public boolean ignoreSysErr()
+        {
             return false;
         }
 
         @Override
-        public boolean ignoreError() {
+        public boolean ignoreError()
+        {
             return false;
         }
     };

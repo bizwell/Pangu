9a35fa492d666cad8e301d3696578312c9e97b7d
songxiang <songxiang@niwodai.net>
2017-08-04 11:27:12 +0800

    解决插件不能打包的问题

--- basic/src/main/java/com/joindata/inf/common/basic/exceptions/BizException.java
+++ basic/src/main/java/com/joindata/inf/common/basic/exceptions/BizException.java
@@ -2,10 +2,12 @@ package com.joindata.inf.common.basic.exceptions;
 
 import com.joindata.inf.common.basic.entities.ErrorEntity;
 
-public class BizException extends RuntimeException {
+public class BizException extends RuntimeException
+{
     private static final long serialVersionUID = 8564997768294356109L;
 
-    public ErrorEntity getErrorEntity() {
+    public ErrorEntity getErrorEntity()
+    {
         return ErrorEntity.define(code, message);
     }
 
@@ -13,23 +15,31 @@ public class BizException extends RuntimeException {
 
     private String message;
 
-    public BizException(int code, String message) {
+    public BizException(int code, String message)
+    {
         this.code = code;
         this.message = message;
     }
 
-    public BizException(ErrorEntity entity) {
+    public BizException(ErrorEntity entity)
+    {
         this.code = entity.getCode();
         this.message = entity.getMessage();
     }
 
-    public BizException(String message) {
+    public BizException(String message)
+    {
         super();
         this.message = message;
     }
 
+    public BizException()
+    {
+    }
+
     @Override
-    public String getMessage() {
+    public String getMessage()
+    {
         return this.message;
     }
 

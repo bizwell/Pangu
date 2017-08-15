4d4c871978388209f8837f580ec779f57fa9da0b
gaowei <gaowei1@niwodai.net>
2017-08-15 18:12:18 +0800

    bug 修复

--- dubbo-support/src/main/java/com/joindata/inf/common/support/dubbo/filter/LogTraceFilter.java
+++ dubbo-support/src/main/java/com/joindata/inf/common/support/dubbo/filter/LogTraceFilter.java
@@ -45,6 +45,7 @@ public class LogTraceFilter implements Filter
 
                 attachment.put(requestIdKey, reqId);
             }
+            return invoker.invoke(invocation);
         }
         catch(RpcException e)
         {
@@ -55,6 +56,5 @@ public class LogTraceFilter implements Filter
         {
             throw new RpcException(e.getMessage(), e);
         }
-        return invoker.invoke(invocation);
     }
 }

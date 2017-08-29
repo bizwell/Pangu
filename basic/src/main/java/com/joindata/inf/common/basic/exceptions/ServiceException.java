fc005c9664450ee59d6fcf3a6bf17540b76fed1b
songxiang <songxiang@niwodai.net>
2017-08-29 15:37:44 +0800

    1.2.4 封版

--- basic/src/main/java/com/joindata/inf/common/basic/exceptions/ServiceException.java
+++ basic/src/main/java/com/joindata/inf/common/basic/exceptions/ServiceException.java
@@ -3,51 +3,61 @@ package com.joindata.inf.common.basic.exceptions;
 import com.joindata.inf.common.basic.entities.ErrorEntity;
 
 /**
- * Created by zhugongyi on 2017/8/22.
- * 业务异常，调用方必须显式处理
+ * Created by zhugongyi on 2017/8/22. 业务异常，调用方必须显式处理
  */
-public class ServiceException extends Exception {
+public class ServiceException extends Exception
+{
+    private static final long serialVersionUID = 4978058763771235892L;
 
     private int code;
 
-    public ServiceException(String message) {
+    public ServiceException(String message)
+    {
         super(message);
     }
 
-    public ServiceException(int code, String message) {
+    public ServiceException(int code, String message)
+    {
         this(message);
         this.setCode(code);
     }
 
-    public ServiceException(int code, String message, Throwable cause) {
+    public ServiceException(int code, String message, Throwable cause)
+    {
         super(message, cause);
         this.setCode(code);
     }
 
-    public ServiceException(int code, Throwable cause) {
+    public ServiceException(int code, Throwable cause)
+    {
         super(cause);
         this.setCode(code);
     }
 
-    public ServiceException(String message, Throwable cause){
+    public ServiceException(String message, Throwable cause)
+    {
         super(message, cause);
     }
 
-    public ServiceException(ErrorEntity entity) {
+    public ServiceException(ErrorEntity entity)
+    {
         this(entity.getMessage());
         this.code = entity.getCode();
     }
 
-    public ServiceException(ErrorEntity entity, Throwable cause) {
+    public ServiceException(ErrorEntity entity, Throwable cause)
+    {
         super(entity.getMessage(), cause);
         this.setCode(entity.getCode());
     }
 
-    public int getCode() {
+    public int getCode()
+    {
         return code;
     }
 
-    public void setCode(int code) {
+    public void setCode(int code)
+    {
         this.code = code;
     }
 }

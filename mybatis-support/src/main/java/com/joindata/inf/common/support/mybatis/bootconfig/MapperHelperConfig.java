c731f27912c12c97b9d82c218a0abb2638e6f85c
likanghua <likanghua@niwodai.net>
2017-09-27 19:34:07 +0800

    添加baseMapper

--- mybatis-support/src/main/java/com/joindata/inf/common/support/mybatis/bootconfig/MapperHelperConfig.java
+++ mybatis-support/src/main/java/com/joindata/inf/common/support/mybatis/bootconfig/MapperHelperConfig.java
@@ -1,33 +1,29 @@
 package com.joindata.inf.common.support.mybatis.bootconfig;
 
-import java.util.Properties;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.joindata.inf.common.basic.support.BootInfoHolder;
 import com.joindata.inf.common.support.mybatis.EnableMyBatis;
+import com.joindata.inf.common.support.mybatis.mapper.BaseMapper;
 import com.joindata.inf.common.support.mybatis.support.MapperHelperProps;
 import com.joindata.inf.common.util.basic.ArrayUtil;
 import com.joindata.inf.common.util.log.Logger;
-
-import tk.mybatis.mapper.common.BaseMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
+
+import java.util.Properties;
 
 /**
  * MyBatis 通用 Mapper 设置
- * 
+ *
  * @author <a href="mailto:songxiang@joindata.com">宋翔</a>
  * @date Aug 30, 2017 2:39:44 PM
  */
 @Configuration
-public class MapperHelperConfig
-{
+public class MapperHelperConfig {
     private static final Logger log = Logger.get();
 
     @Bean(name = "mapperHelper")
-    public MapperScannerConfigurer mapperHelper()
-    {
+    public MapperScannerConfigurer mapperHelper() {
         MapperHelperProps props = Util.getMapperHelperProps();
 
         Properties properties = new Properties();
@@ -38,8 +34,7 @@ public class MapperHelperConfig
         MapperScannerConfigurer scan = new MapperScannerConfigurer();
         scan.setSqlSessionFactoryBeanName("sqlSessionFactory"); // 多数据源时，必须配置
         scan.setBasePackage(ArrayUtil.join(Util.getScanPackage()));// 扫描包
-        for(Class<?> clz: Util.getExternalMappers())
-        {
+        for (Class<?> clz : Util.getExternalMappers()) {
             scan.getMapperHelper().registerMapper(clz);
         }
         scan.getMapperHelper().registerMapper(BaseMapper.class);
@@ -50,31 +45,26 @@ public class MapperHelperConfig
         return scan;
     }
 
-    private static final class Util
-    {
-        public static final String[] getScanPackage()
-        {
+    private static final class Util {
+        public static final String[] getScanPackage() {
             // 获取启动类的扫描包
             Class<?> bootClz = BootInfoHolder.getBootClass();
             EnableMyBatis anno = bootClz.getAnnotation(EnableMyBatis.class);
 
-            if(ArrayUtil.isEmpty(anno.value()))
-            {
+            if (ArrayUtil.isEmpty(anno.value())) {
                 return new String[]{bootClz.getPackage().getName()};
             }
 
             return anno.value();
         }
 
-        public static final Class<?>[] getExternalMappers()
-        {
+        public static final Class<?>[] getExternalMappers() {
             // 获取启动类的扫描包
             Class<?> bootClz = BootInfoHolder.getBootClass();
             return bootClz.getAnnotation(EnableMyBatis.class).mappers();
         }
 
-        public static final MapperHelperProps getMapperHelperProps()
-        {
+        public static final MapperHelperProps getMapperHelperProps() {
             Class<?> bootClz = BootInfoHolder.getBootClass();
             return bootClz.getAnnotation(EnableMyBatis.class).mapperHelperProps();
         }

29ceba55e299bf17b24dd309953ad60eccc843e3
likanghua <likanghua@niwodai.net>
2017-09-30 16:16:41 +0800

    linux时间戳转java data

--- bootable/src/main/java/com/joindata/inf/boot/annotation/NewRestStyle.java
+++ bootable/src/main/java/com/joindata/inf/boot/annotation/NewRestStyle.java
@@ -1,6 +1,7 @@
 package com.joindata.inf.boot.annotation;
 
 import com.joindata.inf.boot.mechanism.newrest.WrapperReturnValueMethod;
+import com.joindata.inf.boot.sterotype.RestResponse;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.Documented;
@@ -12,7 +13,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * 启用新的 REST 风格<br />
- * 
+ *
  * @author <a href="mailto:songxiang@joindata.com">宋翔</a>
  * @date Aug 30, 2017 2:07:21 PM
  * @since 1.2.5
@@ -20,8 +21,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
-@Import(value =  WrapperReturnValueMethod.class)
-public @interface NewRestStyle
-{
+@Import(value = WrapperReturnValueMethod.class)
+public @interface NewRestStyle {
     String value() default "0";
+
+    Class[] exclude() default {RestResponse.class};
 }

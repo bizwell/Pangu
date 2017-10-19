69cfdbdf74c2132b9bd33a190a05c83bae259c99
likanghua <likanghua@niwodai.net>
2017-10-19 10:38:08 +0800

    linux时间戳转java data

--- basic/src/main/java/com/joindata/inf/common/basic/errors/BaseErrorCode.java
+++ basic/src/main/java/com/joindata/inf/common/basic/errors/BaseErrorCode.java
@@ -5,14 +5,25 @@ package com.joindata.inf.common.basic.errors;
  */
 public enum BaseErrorCode {
     S401("没有登陆"),
+
+    S412("不支持该客户端"),
+
     S402("第一次登录，要修改密码"),
+
     S403("无权限"),
+
     S404("资源不存在"),
+
     S405("http method不支持"),
+
     S400("参数不正确"),
+
     S409("参数冲突"),
+
     S408("内部程序之外的异常 主要是第三方资源连接  如 mysql,redis统一抛出该错误 如果业务需要特殊处理,开发人员捕获包装成业务异常"),
+
     S500("业务错误");
+
     private String desc;
 
     private BaseErrorCode(String desc) {
@@ -20,9 +31,8 @@ public enum BaseErrorCode {
     }
 
 
-
     @Override
     public String toString() {
         return super.toString();
     }
-}
+    }

95e329834c575f06b9784134df2f15772ce2177f
likanghua <likanghua@bizwell.net>
2017-10-28 14:34:06 +0800

    linux时间戳转java data

--- mybatis-support/src/main/java/com/joindata/inf/common/support/mybatis/support/annotation/EnablePageHelper.java
+++ mybatis-support/src/main/java/com/joindata/inf/common/support/mybatis/support/annotation/EnablePageHelper.java
@@ -1,8 +1,18 @@
 package com.joindata.inf.common.support.mybatis.support.annotation;
 
+import com.joindata.inf.common.basic.annotation.JoindataComponent;
+import com.joindata.inf.common.support.mybatis.ConfigHub;
+
+import java.lang.annotation.*;
+
 /**
  * Created by likanghua on 2017/9/27.
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@JoindataComponent(bind = ConfigHub.class, name = "RDBMS ORM 支持")
 public @interface EnablePageHelper {
     PageHelpProperties[] value() default {};
 
